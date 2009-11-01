4814538531644e49e01b56023db149173d3de426
Viper007Bond <Viper007Bond@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-01 23:06:18 +0000

    v1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/localtime/trunk@169411 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ localtime.php
@@ -0,0 +1,96 @@
+<?php /*
+
+**************************************************************************
+
+Plugin Name:  Local Time
+Plugin URI:   http://www.viper007bond.com/wordpress-plugins/localtime/
+Version:      1.0.0
+Description:  Displays date and times in the user's timezone using Javascript. Heavily based on code from the <a href="http://wordpress.org/extend/themes/p2">P2 theme</a> by <a href="http://automattic.com/">Automattic</a>.
+Author:       Viper007Bond
+Author URI:   http://www.viper007bond.com/
+
+**************************************************************************/
+
+class ViperLocalTime {
+
+	// Class init
+	function ViperLocalTime() {
+		global $wp_locale;
+
+		// Don't do anything if the current theme is P2 as it does it itself
+		if ( !function_exists('esc_html') || defined('P2_JS_URL') || is_admin() )
+			return;
+
+		add_action( 'wp_head',      array(&$this, 'head_javascript') );
+		add_filter( 'the_date',     array(&$this, 'date_filter'), 1, 2 );
+		add_filter( 'get_the_time', array(&$this, 'time_filter'), 1, 2 );
+
+		wp_enqueue_script( 'wp-locale', plugins_url( 'wp-locale.js', __FILE__ ) , array( 'jquery', 'utils' ) );
+
+		// The localization functionality can't handle objects, that's why
+		// we are using poor man's hash maps here -- using prefixes of the variable names
+		$wp_locale_txt = array();
+		
+		foreach( $wp_locale->month as $key => $month ) $wp_locale_txt["month_$key"] = $month;
+		$i = 1;
+		foreach( $wp_locale->month_abbrev as $key => $month ) $wp_locale_txt["monthabbrev_".sprintf('%02d', $i++)] = $month;
+		foreach( $wp_locale->weekday as $key => $day ) $wp_locale_txt["weekday_$key"] = $day;
+		$i = 1;
+		foreach( $wp_locale->weekday_abbrev as $key => $day ) $wp_locale_txt["weekdayabbrev_".sprintf('%02d', $i++)] = $day;
+
+		wp_localize_script( 'wp-locale', 'localtime', $wp_locale_txt );
+	}
+
+
+	// Javascript that does the replacing
+	function head_javascript() { ?>
+<!-- Local Time v1.0.0 by Viper007Bond | http://www.viper007bond.com/wordpress-plugins/localtime/ -->
+<style type="text/css">.hide { display: none; }</style>
+<script type="text/javascript">
+/* <![CDATA[ */
+	jQuery(document).ready(function($) {
+		function LocalTime() {
+			$('span.localtime').each(function() {
+				var t = $(this);
+				var f = t.find('span.localtime-format').html();
+				var d = localtime_locale.parseISO8601( t.find('span.localtime-thetime').html() );
+				if (d) t.html( localtime_locale.date( f, d ) );
+			});
+		}
+		localtime_locale = new wp.locale(localtime);
+		LocalTime();
+	});
+/* ]]> */
+</script>
+<?php
+	}
+
+
+	// Filter for the_date()
+	function date_filter( $the_date, $format ) {
+		$format = ( empty($format) ) ? get_option('date_format') : $format;
+		return $this->add_data( $the_date, $format );
+	}
+
+
+	// Filter for the_time()
+	function time_filter( $the_time, $format ) {
+		$format = ( empty($format) ) ? get_option('time_format') : $format;
+		return $this->add_data( $the_time, $format );
+	}
+
+
+	// Adds addtional HTML that contains the information for the Javascript
+	function add_data( $string, $format ) {
+		return '<span class="localtime">' . $string . '<span class="localtime-thetime hide">' . esc_html( get_post_time( 'Y-m-d\TH:i:s\Z', true ) ) . '</span><span class="localtime-format hide">' . esc_html( $format ) . '</span></span>';
+	}
+}
+
+// Start this plugin
+add_action( 'init', 'ViperLocalTime', 7 );
+function ViperLocalTime() {
+	global $ViperLocalTime;
+	$ViperLocalTime = new ViperLocalTime();
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== Local Time ===
+Contributors: Viper007Bond
+Tags: timezone, localization
+Requires at least: 2.8.5
+Tested up to: 2.9
+Stable tag: trunk
+
+Displays date and times in the user's timezone using Javascript. Heavily based on code from the P2 theme by Automattic.
+
+== Description ==
+
+Displays date and times in the user's timezone using Javascript. No theme modifications are needed.
+
+Heavily based on code from the [P2 theme](http://wordpress.org/extend/themes/p2) by [Automattic](http://automattic.com/).
+
+**Demo**
+
+Check out [my blog](http://www.viper007bond.com/) to try it yourself. All of the dates and times on my blog are PST (GMT-8 with no DST) but will be displayed in your timezone once the page is fully done loading.
+
+== Installation ==
+
+###Updgrading From A Previous Version###
+
+To upgrade from a previous version of this plugin, delete the entire folder and files from the previous version of the plugin and then follow the installation instructions below.
+
+###Installing The Plugin###
+
+Extract all files from the ZIP file, making sure to keep the file structure intact, and then upload it to `/wp-content/plugins/`. Then just visit your admin area and activate the plugin. That's it!
+
+**See Also:** ["Installing Plugins" article on the WP Codex](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins)
+
+== ChangeLog ==
+
+**Version 1.0.0**
+
+* Initial release.
